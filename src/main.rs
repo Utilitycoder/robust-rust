4e763bcdcf466cb68368d5c7e8e71deb70879c05
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-05-25 18:40:37 +0100

    chore: made port dynamic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use robust_rust::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let endpoint = format!("Server running at http://127.0.0.1:{}", port);
+    println!("{}", endpoint);
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,16 +1,17 @@
 use reqwest::Client;
 use robust_rust::run;
+use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
     // We need to bring in `reqwest`
     // to perform HTTP requests against our
     let client = Client::new();
     // Act
     let response = client
-        .get("http://127.0.0.1:8080/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -19,10 +20,18 @@ async fn health_check_works() {
     assert_eq!(Some(19), response.content_length());
 }
 
-
 #[allow(clippy::let_underscore_future)]
-fn spawn_app() {
-    let server = run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    // Retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+
+    // Launch our application as a background task
+    let server = run(listener).expect("Failed to bind address");
 
     let _ = tokio::spawn(server);
+
+    // Return the application address to the caller!
+    format!("http://127.0.0.1:{}", port)
 }
