c87ba9424fd715e0e6838d88e08033d5a8b1af40
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-05-28 19:35:38 +0100

    feat: added observability to the code with log

--- src/startup.rs
+++ src/startup.rs
@@ -1,4 +1,5 @@
 use crate::routes::{health_check, subscribe};
+use actix_web::middleware::Logger;
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -7,6 +8,8 @@ pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io:
     let connection = web::Data::new(connection);
     let server = HttpServer::new(move || {
         App::new()
+            // Middleware logger added here
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection.clone())
