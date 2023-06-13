4c499a9443dfa3f4209abdacaa7316946f30cd9c
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-06-13 08:53:01 +0100

    test: tested valid emails with multiple emails

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -24,6 +24,8 @@ mod tests {
     use claim::{assert_err, assert_ok};
     use fake::faker::internet::en::SafeEmail;
     use fake::Fake;
+    use quickcheck::Gen;
+    use rand::{rngs::StdRng, SeedableRng};
 
     #[test]
     fn empty_string_is_rejected() {
@@ -61,11 +63,20 @@ mod tests {
         assert_err!(SubscriberEmail::parse(email));
     }
 
-    #[test]
-    fn valid_emails_are_parsed_successfully() {
-        let email = SafeEmail().fake();
-        println!("{}", email);
-        assert_ok!(SubscriberEmail::parse(email));
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary(g: &mut Gen) -> Self {
+            let mut rng = StdRng::seed_from_u64(u64::arbitrary(g));
+            let email = SafeEmail().fake_with_rng(&mut rng);
+            Self(email)
+        }
     }
 
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        let ValidEmailFixture(email) = valid_email;
+        SubscriberEmail::parse(email).is_ok()
+    }
 }
