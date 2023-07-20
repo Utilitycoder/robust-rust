69dd01805d27a99f2172fb4aa5bb53c6d104f8e4
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-07-20 17:24:29 +0100

    j'][{?}

--- src/authentication/middleware.rs
+++ src/authentication/middleware.rs
@@ -1,4 +1,7 @@
-use crate::{session_state::TypedSession, utils::{e500, see_other}};
+use crate::{
+    session_state::TypedSession,
+    utils::{e500, see_other},
+};
 use actix_web::body::MessageBody;
 use actix_web::dev::{ServiceRequest, ServiceResponse};
 use actix_web::error::InternalError;
